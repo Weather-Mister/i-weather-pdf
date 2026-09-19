38e1aeb68af98ed7920821fbc52690b147f6eeb4
Weather-Mister <weatherisgood06@gmail.com>
2026-09-20 02:15:30 +0800

    Refresh final editor feature bundle

--- app.js
+++ app.js
@@ -183,12 +183,12 @@
     if (!document.querySelector('link[data-pdf-editor-css]')) {
       const link = document.createElement("link");
       link.rel = "stylesheet";
-      link.href = "./editor.css?v=14";
+      link.href = "./editor.css?v=15";
       link.dataset.pdfEditorCss = "true";
       document.head.appendChild(link);
     }
 
-    state.editorPromise = loadScript("./editor.js?v=13", "iWeatherPDFEditor")
+    state.editorPromise = loadScript("./editor.js?v=14", "iWeatherPDFEditor")
       .then(() => window.iWeatherPDFEditor)
       .catch((error) => {
         state.editorPromise = null;

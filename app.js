189018b5199f0324965a27dd115ba6510450a11b
Weather-Mister <weatherisgood06@gmail.com>
2026-09-20 01:56:21 +0800

    Refresh inline text editor assets

--- app.js
+++ app.js
@@ -180,12 +180,12 @@
     if (!document.querySelector('link[data-pdf-editor-css]')) {
       const link = document.createElement("link");
       link.rel = "stylesheet";
-      link.href = "./editor.css?v=11";
+      link.href = "./editor.css?v=12";
       link.dataset.pdfEditorCss = "true";
       document.head.appendChild(link);
     }
 
-    state.editorPromise = loadScript("./editor.js?v=10", "iWeatherPDFEditor")
+    state.editorPromise = loadScript("./editor.js?v=11", "iWeatherPDFEditor")
       .then(() => window.iWeatherPDFEditor)
       .catch((error) => {
         state.editorPromise = null;
